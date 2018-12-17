d6d35d331168a1ff8cc8139ccd4d5ed574b3d1de
jvt1963 <39003362+jvt1963@users.noreply.github.com>
2018-12-17 19:35:45 +0100

    Update action-turnOnOff-http.py

--- action-turnOnOff-http.py
+++ action-turnOnOff-http.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python2
-from hermes_python.hermes import Hermes
-import urllib2
+from hermes_python.hermes import Hermes 
+import requests
 from devices import device, deviceOn, deviceOff
 
-MQTT_IP_ADDR = "localhost"
-MQTT_PORT = 1883
-MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+MQTT_IP_ADDR = "localhost" 
+MQTT_PORT = 1883 
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT)) 
+
+
+def device_number(current_slot):
+    for x in device:
+        if x == current_slot:
+            break
+    return device.index(x)
+
 
 def intent_received(hermes, intent_message):
     sentence = 'You asked me '
-
     house_room_slot = intent_message.slots.house_room.first()
-
-    for x in device:
-      print(x)
-      if x == house_room_slot.value:
-         nx = device.index(x)
-         break
+    devnum = device_number(house_room_slot.value)
 
     if intent_message.intent.intent_name == 'jaimevegas:turnOn':
-        print('jaimevegas:turnOn')
         sentence += 'to turn on ' + house_room_slot.value
-	rq = deviceOn[nx]
+        r = requests.get(deviceOn[devnum])
+        
     elif intent_message.intent.intent_name == 'jaimevegas:turnOff':
-        print('jaimevegas:turnOff')
         sentence += 'to turn off ' + house_room_slot.value
-	rq = deviceOff[nx]
+        r = requests.get(deviceOff[devnum])
+        
     else:
         return
-
+    
     hermes.publish_end_session(intent_message.session_id, sentence)
-    r = urllib2.urlopen(rq)
-
+    
+    
 with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+ h.subscribe_intents(intent_received).start()
